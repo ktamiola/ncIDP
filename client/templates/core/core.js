620881ca50a66d561f5b50d3402c8f6f870c8f0e
Kamil Tamiola <kamil@alpine-photography.com>
2015-05-06 08:51:40 +0200

    Minor modifications. Corrected the text in the header of the application.

--- client/templates/core/core.js
+++ client/templates/core/core.js
@@ -1,7 +1,8 @@
 // Invoked when template gets rendered
 Template.core.rendered = function () {
 
-	// Enable the cards
+	// Enable database selection
+	// via convinient tabs system
 	$('[data-toggle="tabs"] a').click(function(e) {
 		// prevent the default behavior -> setting the link URL with hashtag
 		e.preventDefault();
@@ -11,7 +12,8 @@ Template.core.rendered = function () {
 		Session.set('database', 'TamiolaDB');
 	});
 
-	// Enable the cards
+	// Initialize the help system
+	// The left help card
 	$('[data-toggle="help"]').click(function(e) {
 		// prevent the default behavior -> setting the link URL with hashtag
 		e.preventDefault();
