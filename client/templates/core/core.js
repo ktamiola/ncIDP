46a2172bd95dccdbfd8813a132678624ade9b5d8
Kamil Tamiola <kamil@alpine-photography.com>
2015-05-06 15:25:54 +0200

    I have disabled measureScrollbar() function as there seems to be problem with using it. I need to investigate the structure of the template file.

--- client/templates/core/core.js
+++ client/templates/core/core.js
@@ -241,7 +241,7 @@ Template.core.rendered = function () {
     p.toggleBodyScrolling = function() {
         clearTimeout(this._timer);
         if ($('.offcanvas-pane.active').length > 0 && this._useBackdrop) {
-            var scrollbarWidth = this.measureScrollbar();
+            var scrollbarWidth = 100; // this.measureScrollbar();
             var bodyPad = parseInt(($('body').css('padding-right') || 0), 10);
             if (scrollbarWidth !== bodyPad) {
                 $('body').css('padding-right', bodyPad + scrollbarWidth);
@@ -294,5 +294,7 @@ Template.core.rendered = function () {
         $('body')[0].removeChild(scrollDiv);
         return scrollbarWidth;
     };
+
+    window.help = new AppOffcanvas;
 
 }
