570fa4612f322ae6c62c1db927b96d2d0eba19bc
Kamil Tamiola <kamil@alpine-photography.com>
2015-05-06 14:40:58 +0200

    I am attempting to enable the ink-reaction effect, while working with the off canvas UI

--- client/templates/core/core.js
+++ client/templates/core/core.js
@@ -2,24 +2,24 @@
 Template.core.rendered = function () {
 
 	// Ink reaction
-	// $('.ink-reaction').on('click', function(e) {
-	// 	var bound = $(this).get(0).getBoundingClientRect();
-	// 	var x = e.clientX - bound.left;
-	// 	var y = e.clientY - bound.top;
-	// 	var color = o.getBackground($(this));
-	// 	var inverse = (o.getLuma(color) > 183) ? ' inverse' : '';
-	// 	var ink = $('<div class="ink' + inverse + '"></div>');
-	// 	var btnOffset = $(this).offset();
-	// 	var xPos = e.pageX - btnOffset.left;
-	// 	var yPos = e.pageY - btnOffset.top;
-	// 	ink.css({
-	// 		top: yPos,
-	// 		left: xPos
-	// 	}).appendTo($(this));
-	// 	window.setTimeout(function() {
-	// 		ink.remove();
-	// 	}, 1500);
-	// });
+	$('.ink-reaction').on('click', function(e) {
+		var bound = $(this).get(0).getBoundingClientRect();
+		var x = e.clientX - bound.left;
+		var y = e.clientY - bound.top;
+		var color = o.getBackground($(this));
+		var inverse = (o.getLuma(color) > 183) ? ' inverse' : '';
+		var ink = $('<div class="ink' + inverse + '"></div>');
+		var btnOffset = $(this).offset();
+		var xPos = e.pageX - btnOffset.left;
+		var yPos = e.pageY - btnOffset.top;
+		ink.css({
+			top: yPos,
+			left: xPos
+		}).appendTo($(this));
+		window.setTimeout(function() {
+			ink.remove();
+		}, 1500);
+	});
 
 	// =========================================================================
 	// CHEMICAL SHIFT DATABASE SELECTION VIA TABS
@@ -74,19 +74,19 @@ Template.core.rendered = function () {
 
 	// Assign button functionalities
 	// Help button for the chemical shift databases
-	$('[data-toggle="help"]').click(function(e) {
-		// prevent the default behavior -> setting the link URL with hashtag
-		e.preventDefault();
-		// display the off canvas help
-		console.log("Help...");
-	});
+	// $('[data-toggle="help"]').click(function(e) {
+	// 	// prevent the default behavior -> setting the link URL with hashtag
+	// 	e.preventDefault();
+	// 	// display the off canvas help
+	// 	console.log("Help...");
+	// });
 
 	// Help button for the extra offset correction options
-	$('[data-toggle="help2"]').click(function(e) {
-		// prevent the default behavior -> setting the link URL with hashtag
-		e.preventDefault();
-		// display the off canvas help
-		console.log("Help2...");
-	});
+	// $('[data-toggle="help2"]').click(function(e) {
+	// 	// prevent the default behavior -> setting the link URL with hashtag
+	// 	e.preventDefault();
+	// 	// display the off canvas help
+	// 	console.log("Help2...");
+	// });
 
 }
