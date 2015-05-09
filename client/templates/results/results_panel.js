adc4c4a19589d4334587f1cfa386dbbf326c5966
Kamil Tamiola <kamil@alpine-photography.com>
2015-05-09 17:11:07 +0200

    Massive number of improvements, including the addition of FileSaver library.

--- client/templates/results/results_panel.js
+++ client/templates/results/results_panel.js
@@ -1,43 +1,47 @@
 // Helpers
 Template.results_panel.helpers ({
 
-    database : function () {
-        var database = DATABASE;
-        database = database.substring(0, database.length-4);
-        return database;
-    },
+	database : function () {
+		var database = DATABASE;
+		database = database.substring(0, database.length-4);
+		return database;
+	},
 
-    length : function () {
-        return SEQUENCE.length;
-    },
+	length : function () {
+		return SEQUENCE.length;
+	},
 
-    hc : function () {
-        return HC_RATIO.toFixed(2);
-    },
+	hc : function () {
+		if (HC_RATIO > 0.0) {
+			return HC_RATIO.toFixed(2);
+		} else {
+			return 'Unknown';
+		}
+	},
 
-    HA : function () {
-    	return HA_OFFSET.toFixed(2);
-    },
+	HA : function () {
+		return HA_OFFSET.toFixed(2);
+	},
 
-    H : function () {
-    	return H_OFFSET.toFixed(2);
-    },
+	H : function () {
+		return H_OFFSET.toFixed(2);
+	},
 
-    N : function () {
-    	return N_OFFSET.toFixed(2);
-    },
+	N : function () {
+		return N_OFFSET.toFixed(2);
+	},
 
-    CB : function () {
-    	return CB_OFFSET.toFixed(2);
-    },
+	CB : function () {
+		return CB_OFFSET.toFixed(2);
+	},
 
-    CA : function () {
-    	return CA_OFFSET.toFixed(2);
-    },
+	CA : function () {
+		return CA_OFFSET.toFixed(2);
+	},
 
-    C : function () {
-    	return C_OFFSET.toFixed(2);
-    }
+	C : function () {
+		return C_OFFSET.toFixed(2);
+	}
 
 
 });
